f1c08619b7f056882c4741a801b4501f69c25d64
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MBP.codaisseur.local>
2022-05-10 10:02:19 +0200

    user details

--- src/store/user/slice.js
+++ src/store/user/slice.js
@@ -2,8 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   name: "David",
-  email: "david.rajcher@gmail.com",
-  id: "9dsajkdakj42aedsa23a",
+  email: "a@a.com",
+  id: "4",
   posts: {
     upvoted: [1, 6, 7, 22, 57, 33, 61, 23],
     downvoted: [2, 8, 44, 87, 64, 24, 41, 62],
