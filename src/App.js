b14d38a3e71c08c301922f668ad79c9a8b46dacf
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MBP.codaisseur.local>
2022-05-10 11:31:56 +0200

    completed lesson 1 tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import { useSelector } from "react-redux";
-import { selectUser } from "./store/user/selector";
+import {
+  selectSavedPosts,
+  selectUser,
+  selectUserPosts,
+} from "./store/user/selector";
 import { selectComments } from "./store/comments/selector";
 import { selectPosts } from "./store/posts/selector";
+import { user } from "./store/user/slice";
 
 function App() {
-  const comments = useSelector(selectComments);
-  const user = useSelector(selectUser);
-  const posts = useSelector(selectPosts);
+  // const comments = useSelector(selectComments);
+  // const user = useSelector(selectUser);
+  // const posts = useSelector(selectPosts);
 
-  console.log({ comments, user, posts });
+  const userPosts = useSelector(selectUserPosts);
 
-  return <div className='App'>Hello world</div>;
+  const savedPosts = useSelector(selectSavedPosts);
+
+  console.log({ savedPosts });
+
+  return (
+    <div className='App'>
+      <div>
+        <h1>Welcome {userPosts.name}</h1>
+        <h2>Upvoted</h2>
+        {userPosts.posts.upvoted.map((upvote) => (
+          <div>
+            <p>{upvote.title}</p>
+            <p>{upvote.body}</p>
+          </div>
+        ))}
+        <h2>Downvoted</h2>
+        {userPosts.posts.downvoted.map((upvote) => (
+          <div>
+            <p>{upvote.title}</p>
+            <p>{upvote.body}</p>
+          </div>
+        ))}
+        <h2>Saved</h2>
+        {userPosts.posts.saved.map((upvote) => (
+          <div>
+            <p>{upvote.title}</p>
+            <p>{upvote.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/store/user/selector.js
+++ src/store/user/selector.js
@@ -1,1 +1,39 @@
 export const selectUser = (reduxState) => reduxState.user;
+
+// fetch the user
+// fetch posts that the user upvote, downvote and saved
+
+export const selectUserPosts = (reduxState) => {
+  const { user, posts } = reduxState;
+
+  const upvoted = user.posts.upvoted.map((upvote) =>
+    posts.find((post) => post.id === upvote)
+  );
+
+  const downvoted = user.posts.downvoted.map((downvote) =>
+    posts.find((post) => post.id === downvote)
+  );
+
+  const saved = user.posts.saved.map((save) =>
+    posts.find((post) => post.id === save)
+  );
+
+  return {
+    ...user,
+    posts: {
+      upvoted,
+      downvoted,
+      saved,
+    },
+  };
+};
+
+export const selectSavedPosts = (reduxState) => {
+  const { user, posts } = reduxState;
+
+  const saved = user.posts.saved.map((save) =>
+    posts.find((post) => post.id === save)
+  );
+
+  return saved;
+};
